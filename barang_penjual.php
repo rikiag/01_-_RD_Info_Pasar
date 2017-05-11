78470ba196f5d1c1eef7505366c36923d2b53564
Riki Agusnaidi <r.agusnaidi@gmail.com>
2017-05-11 16:57:16 +0700

    mengambil data session dari login

--- barang_penjual.php
+++ barang_penjual.php
@@ -11,43 +11,41 @@
   <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
-<body>
+<body class="grey lighten-4">
   <?php include 'menu.php'; ?>
-  <div class="grey lighten-4">
-    <div class="container">
-      <div class="section">
-        <div class="row">
-          <?php 
-            $telp = $_GET['telp'];
-            $sql = mysqli_query($con,"SELECT * FROM `barang` JOIN barang_user ON barang.id_barang = barang_user.id_barang JOIN user ON user.no_telp = barang_user.no_telp AND user.no_telp = '$telp'");
-            while ($data = mysqli_fetch_object($sql)):
+  <div class="container">
+    <div class="section">
+      <div class="row">
+        <?php
+        $telp = $_SESSION['no_telp'];
+        $sql = mysqli_query($con,"SELECT * FROM `barang` JOIN barang_user ON barang.id_barang = barang_user.id_barang JOIN user ON user.no_telp = barang_user.no_telp AND user.no_telp = '$telp'");
+        while ($data = mysqli_fetch_object($sql)):
           ?>
-          <div class="col s12 m6 l4">
-            <div class="card horizontal">
-              <div class="card-image">
-                <img src="images/<?php echo $data->foto_barang; ?>">
+        <div class="col s12 m6 l4">
+          <div class="card horizontal">
+            <div class="card-image">
+              <img src="images/<?php echo $data->foto_barang; ?>">
+            </div>
+            <div class="card-stacked">
+              <div class="card-content">
+                <p><?php echo $data->nama_barang; ?></p>
               </div>
-              <div class="card-stacked">
-                <div class="card-content">
-                  <p><?php echo $data->nama_barang; ?></p>
-                </div>
-                <div class="card-action">
-                  <a href="barang.php?id_barang=<?php echo $data->id_barang; ?>">Rp. <?php echo number_format($data->harga_sekarang,2,",","."); ?></a>
-                </div>
+              <div class="card-action">
+                <a href="barang.php?id_barang=<?php echo $data->id_barang; ?>">Rp. <?php echo number_format($data->harga_sekarang,2,",","."); ?></a>
               </div>
             </div>
           </div>
-        <?php endwhile; ?>
         </div>
-      </div>
+      <?php endwhile; ?>
     </div>
   </div>
+</div>
 
-  <!--  Scripts-->
-  
-  <script src="js/jquery-2.1.1.min.js"></script>
-  <script src="js/materialize.js"></script>
-  <script src="js/init.js"></script>
+<!--  Scripts-->
+
+<script src="js/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
 
 </body>
 </html>
--- menu.php
+++ menu.php
@@ -1,38 +1,46 @@
-  <div class="navbar-fixed">
-    <nav class="light-blue lighten-1" role="navigation">
-      <div class="nav-wrapper container">
-        <a href="#" class="brand-logo" data-activates="slide-out">Pasar Rukoh</a>
-        <!-- Menu in Desktop mode -->
-        <ul class="right hide-on-med-and-down">
-          <li><a href="#">Navbar Link</a></li>
-        </ul>
-        <ul class="right hide-on-med-and-down">
-          <li><a href="#">Navbar Link</a></li>
-        </ul>
+<?php 
+session_start();
+?>
+<div class="navbar-fixed">
+  <nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container">
+      <a href="#" class="brand-logo" data-activates="slide-out">Pasar Rukoh</a>
+      <!-- Menu in Desktop mode -->
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Navbar Link</a></li>
+      </ul>
 
-        <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-      </div>
-    </nav>
-  </div>
-  <div class="nav-wrapper container">
-    <!-- Menu in Mobile mode -->
-    <ul id="slide-out" class="side-nav">
-      <li>
-        <div class="userView">
-          <div class="background">
-            <img src="images/office.jpg">
-          </div>
-          <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
-          <a href="#!name"><span class="white-text name">John Doe</span></a>
-          <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+      <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+</div>
+<div class="nav-wrapper container">
+  <!-- Menu in Mobile mode -->
+  <ul id="slide-out" class="side-nav">
+    <li>
+      <div class="userView">
+        <div class="background">
+          <img src="images/office.jpg">
         </div>
-      </li>
-      <li><a class="waves-effect" href="index.html">Home</a></li>
-      <li><a class="waves-effect" href="barang_penjual.php">Barang</a></li>
-      <li><a class="waves-effect" href="#!">Edit Profil</a></li>
-      <li><a class="waves-effect" href="#!">Jasa Antar Barang</a></li>
-      <li><a class="waves-effect" href="#!">About</a></li>
-      <li><div class="divider"></div></li>
-      <li><a class="waves-effect" href="#!">Logout</a></li>
-    </ul>
-  </div>
+        <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
+        <a href="#!name"><span class="white-text name">John Doe</span></a>
+        <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+      </div>
+    </li>
+    <li><a class="waves-effect" href="index.html">Home</a></li>
+    <li><a class="waves-effect" href="barang_penjual.php">Barang</a></li>
+    <li><a class="waves-effect" href="#!">Edit Profil</a></li>
+    <li><a class="waves-effect" href="#!">Jasa Antar Barang</a></li>
+    <li><a class="waves-effect" href="#!">About</a></li>
+    <!-- MENU LOGIN LOGOUT -->
+    <li><div class="divider"></div></li>
+    <?php if (isset($_SESSION['no_telp'])): ?>
+      <li><a class="waves-effect" href="logout.php">Logout</a></li>
+    <?php else: ?>
+      <li><a class="waves-effect" href="login.php">Login</a></li>
+    <?php endif; ?>
+  </ul>
+</div>
