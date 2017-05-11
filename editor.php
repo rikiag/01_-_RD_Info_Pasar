e89c2ef8037c6ffe573e0d9ee1e294d10574c5d3
mahjati amanda <mahjati.amanda14@gmail.com>
2017-05-11 23:08:20 +0700

    amanda

---
+++ editor.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<HTML>
+  <head>
+    <title>update</title>
+  </head>
+  <style>
+  body {
+   background-size:100%;
+   background-image:url("images/upload.jpg");
+   background-position:right top;
+   background-attachment:fixed;
+ 
+}
+</style>
+	
+	
+
+<body>
+<center>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    harga: <input type="text" name="harga">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+
+
+
+
+	</center>
+	</body>
+	</HTML>
+
