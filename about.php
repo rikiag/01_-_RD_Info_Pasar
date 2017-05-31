f3f02428a37fef1ac195f53adc22a0d371495381
Annisa Mahfira <annisamahfira97@gmail.com>
2017-05-31 14:23:26 +0700

    nama developer

---
+++ about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Kelompok 1</title>
+
+  <!-- CSS  -->
+  <link href="fonts/icon.css" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body class="grey lighten-4">
+<?php include 'menu.php'; ?>
+  <div class="container">
+    <div class="section">
+      <div class="row">
+        <div class="card cyan lighten-5">
+          <div class="card-content">
+          <p>Aplikasi ini dibuat untuk tugas proyek final pada mata kuliah Rekayasa Perangkat Lunak, Jurusan Informatika Universitas Syiah Kuala</p>
+          </div>
+        </div>
+        <ul class="collection">
+          <li class="collection-item avatar">
+            <img src="images/user.ico" alt="" class="circle">
+            <span class="title">Riki Agusnaidi</span>
+            <p>1608107010053 <br>
+            </p>
+          </li>
+          <li class="collection-item avatar">
+            <img src="images/user.ico" alt="" class="circle">
+            <span class="title">M. Ilham Surya Alam</span>
+            <p>1508107010013 <br>
+            </p>
+          </li>
+          <li class="collection-item avatar">
+            <img src="images/user.ico" alt="" class="circle">
+            <span class="title">Aida Ristia</span>
+            <p>1508107010019 <br>
+            </p>
+          </li>
+          <li class="collection-item avatar">
+            <img src="images/user.ico" alt="" class="circle">
+            <span class="title">Shinta Keumala Sari</span>
+            <p>1508107010029 <br>
+            </p>
+          </li>
+          <li class="collection-item avatar">
+            <img src="images/user.ico" alt="" class="circle">
+            <span class="title">Annisa Mahfira</span>
+            <p>1508107010030 <br>
+            </p>
+          </li>
+          <li class="collection-item avatar">
+            <img src="images/user.ico" alt="" class="circle">
+            <span class="title">Mahjati Amanda</span>
+            <p>1508107010038 <br>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+<!--  Scripts-->
+<script src="js/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
+
+</body>
+</html>
